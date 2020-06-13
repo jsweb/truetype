5d7cc0e6fe5071a619a03fae6619026c054a69a6
Alex Bruno Cáceres <alxbr@dell-insp-w10.localdomain>
2020-06-13 17:00:12 -0300

    NVM env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @desc Simple JS module to check types more consistently
  * @author Alex Bruno <git.alexbr@outlook.com>
  * @create date 2016-06-26 03:21:18
- * @modify date 2020-06-13 19:20:39
+ * @modify date 2020-06-13 20:00:15
  */
 /**
  * Check if a value is not undefined
